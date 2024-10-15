62ca5f741cb961d6299ac26902eb8396496c3061
ilmys <136291727+ilmys@users.noreply.github.com>
2024-10-15 20:53:42 +0700

    Update display.js

--- src/display.js
+++ src/display.js
@@ -1,11 +1,10 @@
 function displayHeader() {
   process.stdout.write('\x1Bc');
-  console.log('
-╔╗ ╦  ╦ ╦╔╦╗  ╔╗ ╔═╗╔╦╗
-╠╩╗║  ║ ║║║║  ╠╩╗║ ║ ║ 
-╚═╝╩═╝╚═╝╩ ╩  ╚═╝╚═╝ ╩ 
-'.cyan);
- console.log();
+  console.log('╔╗ ╦  ╦ ╦╔╦╗  ╔╗ ╔═╗╔╦╗'.green);
+  console.log('╠╩╗║  ║ ║║║║  ╠╩╗║ ║ ║ '.green);
+  console.log('╚═╝╩═╝╚═╝╩ ╩  ╚═╝╚═╝ ╩ '.green);
+  console.log('by:signalrekt'.cyan);
+  console.log();
 }
 
 module.exports = { displayHeader };
